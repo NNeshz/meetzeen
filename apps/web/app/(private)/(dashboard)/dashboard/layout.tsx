c29a440d12e9aeaa53f1349364e38bf5065deb6f
Neftali Hernandez <neftaligow01@gmail.com>
2025-12-05 16:10:49 -0600

    💄 (style): Dashboard
    Muck up of the dashboard with the feature of log out working

---
+++ apps/web/app/(private)/(dashboard)/dashboard/layout.tsx
@@ -0,0 +1,38 @@
+import { Metadata } from "next";
+
+import { AppSidebar } from "@/modules/dashboard/common/dashboard-sidebar";
+import {
+  SidebarProvider,
+  SidebarInset,
+  SidebarTrigger,
+} from "@meetzeen/ui/components/sidebar";
+import { Separator } from "@meetzeen/ui/src/components/separator";
+
+export const metadata: Metadata = {
+  title: "Dashboard | Meetzeen",
+  description: "Dashboard de Meetzeen",
+};
+
+export default function AdminLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <SidebarProvider>
+      <AppSidebar />
+      <SidebarInset>
+        <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
+          <div className="flex items-center gap-2 px-4">
+            <SidebarTrigger className="-ml-1" />
+            <Separator
+              orientation="vertical"
+              className="mr-2 data-[orientation=vertical]:h-4"
+            />
+          </div>
+        </header>
+        <div className="flex flex-1 flex-col gap-4 p-4 pt-0">{children}</div>
+      </SidebarInset>
+    </SidebarProvider>
+  );
+}
